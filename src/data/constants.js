cb28df160b5e424c07695adc5232637a0e88bf35
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-11-07 11:57:50 -0300

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
   //   "",
   github: "https://github.com/VictorRibeiroH",
   resume:
-    "https://drive.google.com/file/d/1599Qphr-BzOB3g7BeGKAsficpgtLneb6/view?usp=sharing",
+    "https://drive.google.com/file/d/15QseZEBCpddmtyuwr5MooNdIE9O9R0sa/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/victor-ribeiroh/",
   insta: "https://www.instagram.com/victxribeiro/"
 };
